ad866bbf2b546eb0ca2a7953cccf6a2901caf2cd
Charlie Arriaga <arriayaa@gmail.com>
2023-12-05 18:12:43 -0800

    worked on request

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ app.use(cors());
 const PORT = process.env.PORT;
 
 app.get('/weather', (request, response) => {
+    let query = request.query;
     let responseData;
-    responseData = data.find(function (element) {
-        if (element.city_name === request.searchQuery && element.lat === request.lat && element.lon === request.lon) {
+    responseData = data.find((element) => {
+        if (element.city_name === query.searchQuery && element.lat === query.lat && element.lon === query.lon) {
             return element;
         } else {
             return 'Error, cannot find city';
@@ -27,16 +28,16 @@ app.get('/weather', (request, response) => {
     class Forecast {
 
         constructor(element) {
-            this.date = element.valid_date;
+            this.date = element[1].valid_date;
 
-            this.description = (`A high of ${element.max_temp}, low of ${element.low_temp} with ${element.weather.description}`);
+            this.description = (`A high of ${element[1].max_temp}, low of ${element[1].low_temp} with ${element[1].weather.description}`);
         }
     }
 
 
     const DaysArr = responseArr.map((val)=> {
-        console.log(val[1]);
-        val = new Forecast(val[1]);
+        console.log(val);
+        val = new Forecast(val);
         return val;
     });
 
