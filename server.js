0711365e3d693e2cb58215bbe4910834861ed649
Charlie Arriaga <arriayaa@gmail.com>
2023-12-08 07:17:18 -0800

    set up weather data transer

--- server.js
+++ server.js
@@ -2,57 +2,66 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const data = require('./data/weather.json');
 const axios = require('axios');
 
 const app = express();
-
 app.use(cors());
 
 const PORT = process.env.PORT;
 const WEATHERAPI = process.env.WEATHER_API_KEY;
-
-async function getWeather() { }
 
 app.get('/weather', async function (request, response) {
     let lat = request.query.lat;
     let lon = request.query.lon;
     let responseData;
 
-    const url = `http://api.weatherbit.io/v2.0/current?${WEATHERAPI}&lang=en&lat=${lat}&lon=${lon}`;
+    const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${WEATHERAPI}`;
 
     try {
         responseData = await axios.get(url);
-        console.log(responseData);
 
     } catch (error) {
         console.log('error');
     }
 
+    console.log(responseData.data.data[0].temp);
 
-    //ChatGPT was consulted for line 24
-    const responseArr = Object.entries(responseData.data);
-
-    class Forecast {
-
-        constructor(element) {
-            this.date = element[1].valid_date;
-
-            this.description = (`A high of ${element[1].max_temp}, low of ${element[1].low_temp} with ${element[1].weather.description}`);
+    function description(responseArr) {
+        let descriptionStrings = [];
+        if (responseArr.clouds < 40) {
+            descriptionStrings.push('Clear skies');
+        } else if ((responseArr.clouds > 40) && (responseArr.clouds < 70)) {
+            descriptionStrings.push('Some clouds');
+        } else {
+            descriptionStrings.push('Cloudy');
         }
+        if (responseArr.precip < 20) {
+            descriptionStrings.push(' no rain');
+        } else if ((responseArr.precip > 20) && (responseArr.precip < 40)) {
+            descriptionStrings.push(' some rain');
+        } else {
+            descriptionStrings.push(' rainy');
+        }
+        if (responseArr.snow <20) {
+            descriptionStrings.push(' with no snow.');
+        } else {
+            descriptionStrings.push(' and snowing.');
+        }
+        return descriptionStrings;
     }
 
+    let describe = description(responseData.data.data[0]).join('');
 
-    const DaysArr = responseArr.map((val) => {
-        console.log(val);
-        val = new Forecast(val);
-        return val;
-    });
+    function Forecast(element) {
+        this.date = element.data.data[0].ob_time;
 
-    response.send(DaysArr);
+        this.description = (`It is ${element.data.data[0].temp}. ${description(describe)} Sunrise is at ${element.data.data[0].sunrise}, and sunset is at ${element.data.data[0].sunset}.`);
+    }
+    const val = new Forecast(responseData);
+
+    console.log(val);
+    response.send(val);
 
 });
-
-getWeather();
 
 app.listen(PORT, () => console.log('server is listening'));
